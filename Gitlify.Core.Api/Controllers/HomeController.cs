3da4ada9335a6ad993b1e5a12f34a4793851119a
Yahyo <96131319+yahyoHakimov@users.noreply.github.com>
2024-06-13 10:14:03 +0500

    Create README.md

---
+++ Gitlify.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Gitlify.Core.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get() =>
+            Ok("Hey Mario, the princess is in another castle.");
+    }
+}
